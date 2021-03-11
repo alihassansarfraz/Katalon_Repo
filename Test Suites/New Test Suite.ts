5fecca253d22e8e8e70523d8f3171f6031d70f25
Ali Hassan <AHassan1@pattersoncompanies.com>
2021-03-11 20:51:03 +0500

    First Commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2b5d2b23-7798-45eb-ad44-193c02d2594a</testSuiteGuid>
+   <testCaseLink>
+      <guid>608a79d8-8fe4-4dc7-896a-b0ff397a10e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
